72849233d349e8ef7bc4a1f669b3077a034a3714
Kelvin Glaß <glass.kelvin@gmx.de>
2023-01-23 22:36:43 +0100

    :recycle: Make functionality callable via go

--- pkg/hcl2diff/hcl2diff.go
+++ pkg/hcl2diff/hcl2diff.go
@@ -1,13 +1,8 @@
-package main
+package hcl2diff
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"os"
-
-	"github.com/jessevdk/go-flags"
-	"github.com/m273d15/hcl2diff/pkg/hcl2diff"
+	"github.com/m273d15/hcl2diff/internal/getter"
+	"github.com/m273d15/hcl2diff/pkg/hcl2json"
 )
 
 const (
@@ -21,17 +16,15 @@ type Hcl2DiffOpts struct {
 	WorkDir        *string  `long:"workdir" description:"Workdir used to copy the source to (default: '$PWD/.temp_hcl2diff')" required:"false"`
 }
 
-func main() {
-	var opts Hcl2DiffOpts
-	_, err := flags.Parse(&opts)
-	if err != nil {
-		os.Exit(1)
+func Hcl2DiffJsonWithGetter(oldSrcVersion, newSrcVersion string, fileExtensions []string, workDir *string) hcl2json.JsonMap {
+	var wd string
+	if workDir == nil {
+		wd = getter.InitWorkdir()
+	} else {
+		wd = getter.InitWorkdirPath(*workDir)
 	}
-
-	jsonPatch := hcl2diff.Hcl2DiffJsonWithGetter(opts.OldSrcVersion, opts.NewSrcVersion, opts.FileExtensions, opts.WorkDir)
-	jsonB, err := json.Marshal(jsonPatch)
-	if err != nil {
-		log.Fatal("Failed to marshal json patch")
-	}
-	fmt.Println(string(jsonB))
+	// TODO: Handle getter issues
+	oldHcl2Files := getter.GetFiles(oldSrcVersion, wd, "oldSrc", fileExtensions)
+	newHcl2Files := getter.GetFiles(newSrcVersion, wd, "newSrc", fileExtensions)
+	return hcl2json.Hcl2DiffJsonMap(oldHcl2Files, newHcl2Files)
 }
